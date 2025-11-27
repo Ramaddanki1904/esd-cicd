4ef427b7bab7d3d8f2480b8db0a023450b7a1812
Ramaddanki1904 <ramadddanki1904@gmail.com>
2025-11-28 00:22:59 +0530

    Added Docker setup and updated EmployeeController

--- ems-backend/ems-backend/src/main/java/com/employeesystem/emsbackend/controller/EmployeeController.java
+++ ems-backend/ems-backend/src/main/java/com/employeesystem/emsbackend/controller/EmployeeController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@CrossOrigin(origins = "http://localhost:5173")  // ✅ FIXED CORS
+@CrossOrigin(origins = "http://localhost:5173")  
 @RestController
 @RequestMapping("/api/emp")
 @AllArgsConstructor
